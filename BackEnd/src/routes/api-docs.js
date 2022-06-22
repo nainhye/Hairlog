6c34c3726c31110b5f566a68fed1990f3c049abd
belljun3395 <195850@jnu.ac.kr>
2022-06-22 14:13:52 +0900

    Swagger 분리

--- BackEnd/src/routes/api-docs.js
+++ BackEnd/src/routes/api-docs.js
@@ -2,7 +2,7 @@ var express = require('express'),
     router = express.Router();
 
     
-const Swagger = require('../../swagger/Swagger.js');
+const Swagger = require('../../../Swagger/Swagger.js');
 
 router.use('/', Swagger.Ui.serve, Swagger.Ui.setup(Swagger.specs, { explorer : true}));
 
